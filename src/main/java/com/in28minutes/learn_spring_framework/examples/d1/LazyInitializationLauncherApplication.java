4bc5f4a3554ee4dbff452162a9535c871f4f70fd
test <test@test-ThinkStation-P520c>
2026-02-03 17:51:25 +0530

    msg

---
+++ src/main/java/com/in28minutes/learn_spring_framework/examples/d1/LazyInitializationLauncherApplication.java
@@ -0,0 +1,42 @@
+package com.in28minutes.learn_spring_framework.examples.d1;
+
+import java.util.Arrays;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+class ClassA{
+	
+}
+
+@Component
+@Lazy
+class ClassB{
+	private ClassA classA;
+
+	public ClassB(ClassA classA) {
+		System.out.println("Some Initialization logic");
+	   this.classA=classA;
+	}
+	
+}
+
+@Configuration
+@ComponentScan("com.in28minutes.learn_spring_framework.examples.d1")
+public class LazyInitializationLauncherApplication {
+	
+	
+	public static void main(String[]args) {
+		try (var context = new AnnotationConfigApplicationContext
+				(LazyInitializationLauncherApplication.class)) {
+			System.out.println("Initialization of context is completed");
+			Context.getBean(ClassB.class);
+		}
+	}
+}
